a4bb2c3564cbb2b239b9f21d395e21644490eeee
HaykSeropyan <127044533+HaykSeropyan@users.noreply.github.com>
2023-03-05 09:15:22 -0800

    Add files via upload

---
+++ script.py
@@ -0,0 +1,28 @@
+import re
+import csv
+csv_location = '/home/hyco/Desktop/email_domain_changer/1millionemails.csv'
+domain = r'@yahoo.com$'
+new_domain = r'@haykdomain.com'
+new_file_location = '/home/hyco/Desktop/email_domain_changer/new_emails.csv'
+old_email_list = []
+
+    
+
+with open(new_file_location,'w') as new_file:
+    data = csv.writer(new_file)
+
+    with open(csv_location,'r') as csv_file:
+        emails = csv.reader(csv_file)
+        for i in emails:
+            if re.search(domain,i[0]):
+                result = re.sub(domain,new_domain,i[0])
+                data.writerow(result.split(','))
+            else:
+                data.writerow(i)
+
+
+        
+
+
+
+
